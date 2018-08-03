e3c42c3c24f9bc99a5a3177a97099c93e0c25dd2
betta <l21828604@gmail.com>
2018-08-03 17:39:01 +0800

    dns scan

--- scan.py
+++ scan.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import sys
 import asyncio
 import aiodns
 
-loop = asyncio.get_event_loop()
-resolver = aiodns.DNSResolver(loop=loop)
-f = resolver.query('baidu.com', 'A')
-result = loop.run_until_complete(f)
+tasks = []
 
-print(result)
+
+def main():
+    print(sys.argv[1])
+    domain = sys.argv[1]
+
+    loop = asyncio.get_event_loop()
+
+    with open('./bitquark_20160227_subdomains_popular_1000') as f:
+        for domain_keyword in f:
+            domain_keyword = domain_keyword.strip()
+            subdomain = domain_keyword+'.'+domain
+
+            try:
+                resolver = aiodns.DNSResolver(loop=loop)
+                f = resolver.query(subdomain, 'A')
+                result = loop.run_until_complete(f)
+                print(subdomain)
+            except Exception as e:
+                result = "NO DNS"
+
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
